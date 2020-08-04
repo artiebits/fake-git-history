31aba9e9984fdfb2812df873f649132f075622e8
hellomegha <johndoejabra+mekhla@gmail.com>
2020-08-04 13:41:07 -0700

    update the date format

--- cli.js
+++ cli.js
@@ -9,15 +9,15 @@ const cli = meow(
       $ fake-git-history [options]
  
     Options
-      --startDate, -s Start date in YY/MM/DD format.
-      --endDate, -e End date in YY/MM/DD format.
+      --startDate, -s Start date in YYYY/MM/DD format.
+      --endDate, -e End date in YYYY/MM/DD format.
       --workdaysOnly, -w Skip weekends.
       --commitsPerDay, -c The number of commits to generate per day.
       
     Examples
-      $ fake-git-history --startDate <YY/MM/DD> --endDate <YY/MM/DD>
-      $ fake-git-history --commitsPerDay "1,5" -s <YY/MM/DD> -e <YY/MM/DD>
-      $ fake-git-history --workdaysOnly -s <YY/MM/DD> -e <YY/MM/DD>
+      $ fake-git-history --startDate <YYYY/MM/DD> --endDate <YYYY/MM/DD>
+      $ fake-git-history --commitsPerDay "1,5" -s <YYYY/MM/DD> -e <YYYY/MM/DD>
+      $ fake-git-history --workdaysOnly -s <YYYY/MM/DD> -e <YYYY/MM/DD>
 `,
   {
     flags: {
